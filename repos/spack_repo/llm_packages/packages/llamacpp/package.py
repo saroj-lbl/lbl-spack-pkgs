56ae69ebd91e3b6811b69fbc0508b90bc7ba4ba5
Saroj Adhikari <sadhikari@lbl.gov>
2025-08-26 07:04:07 -0700

    args->cmake_args; fix url typo; add CudaPackage base class

--- repos/spack_repo/llm_packages/packages/llamacpp/package.py
+++ repos/spack_repo/llm_packages/packages/llamacpp/package.py
@@ -1,11 +1,13 @@
+from spack_repo.builtin.build_systems.cmake import CMakePackage
+from spack_repo.builtin.build_systems.cuda import CudaPackage
 from spack.package import *
 
-class LlamaCpp(CMakePackage):
+class Llamacpp(CMakePackage, CudaPackage):
     """LLama.cpp package
     """
 
     homepage = "https://github.com/ggml-org/llama.cpp"
-    url = "https://github.comggml-org/llama.cpp/archive/refs/tags/b6276.tar.gz"
+    url = "https://github.com/ggml-org/llama.cpp/archive/refs/tags/b6276.tar.gz"
     git = "https://github.com/ggml-org/llama.cpp.git"
 
     license("MIT")
@@ -13,25 +15,20 @@ class LlamaCpp(CMakePackage):
     version("latest", branch="master")
     version("b6276", tag="b6276")
 
-    # variants
-    variant("cuda", default=False)
-
     # dependencies
     depends_on("cmake@3.14:", type="build")
     depends_on("git", type="build")
-
+    
     def cmake_args(self):
-        args = []
+        cmake_args = []
 
-        args.extend([
+        cmake_args.extend([
             self.define_from_variant("GGML_CUDA", "cuda")
         ])
 
         # cuda architecture
-        if "+cuda" in self.spec:
-            args.extend([
-                self.define_from_variant("")
-            ])
-
+        if self.spec.satisfies("+cuda"):
             if "CMAKE_CUDA_ARCHITECTURES" not in self.spec.variants:
-                args.append("-DCMAKE_CUDA_ARCHITECTURES=70;75;86;90")
+                cmake_args.append("-DCMAKE_CUDA_ARCHITECTURES=70;75;86;90")
+
+        return cmake_args
